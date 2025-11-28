356047d6da87a750477282d754202e648ad20234
Burak AYDIN <burak.aydin@robopine.com>
2025-11-28 23:31:08 +0300

    Initial Commit

---
+++ nfcapp/nfcappApp.swift
@@ -0,0 +1,17 @@
+//
+//  nfcappApp.swift
+//  nfcapp
+//
+//  Created by Recai Çam on 28.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct nfcappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
